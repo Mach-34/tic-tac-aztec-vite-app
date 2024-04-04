0f87d3100b9f3f32f56f358757ac30af4b442530
Ian-Bright <ianbrighton1@gmail.com>
2024-04-04 13:25:29 -0600

    feat: Counter displays cases less than 10 seconds correctly

--- src/hooks/useCountdown.tsx
+++ src/hooks/useCountdown.tsx
@@ -9,7 +9,7 @@ const useCountdown = (expiry: number) => {
     if (expiry > 0) {
       interval = setInterval(() => {
         const newCountdown = calculateCountdown(expiryMilli);
-        if (newCountdown.minutes === 0 && newCountdown.seconds === 0) {
+        if (newCountdown.minutes === '0' && newCountdown.seconds === '00') {
           clearInterval(interval);
         } else {
           setCountdown(newCountdown);
@@ -26,12 +26,15 @@ const calculateCountdown = (expiry: number) => {
   const currentTime = new Date().getTime();
   const difference = expiry - currentTime;
   const remainingTime = Math.min(difference, expiry);
-  let timeLeft = { minutes: 0, seconds: 0 };
+  let timeLeft = { minutes: '0', seconds: '00' };
+
+  const minutes = Math.floor(remainingTime / (1000 * 60));
+  const seconds = Math.floor((remainingTime / 1000) % 60);
 
   if (remainingTime > 0) {
     timeLeft = {
-      minutes: Math.floor(remainingTime / (1000 * 60)),
-      seconds: Math.floor((remainingTime / 1000) % 60),
+      minutes: minutes.toString(),
+      seconds: seconds < 10 ? `0${seconds}` : seconds.toString(),
     };
   }
   return timeLeft;
